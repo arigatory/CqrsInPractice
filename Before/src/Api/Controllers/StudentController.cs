6c2afcc5a9cdf797ac2277c72654b80fff5bb7e3
Ivan MSI <arigatory@gmail.com>
2023-06-16 00:32:30 +0300

    CQS refactor

--- Before/src/Api/Controllers/StudentController.cs
+++ Before/src/Api/Controllers/StudentController.cs
@@ -3,26 +3,18 @@ using Logic.Dtos;
 using Logic.Students;
 using Logic.Utils;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Api.Controllers
 {
     [Route("api/students")]
     public sealed class StudentController : BaseController
     {
-        private readonly UnitOfWork _unitOfWork;
         private readonly Messages _messages;
-        private readonly StudentRepository _studentRepository;
-        private readonly CourseRepository _courseRepository;
 
-        public StudentController(UnitOfWork unitOfWork, Messages messages)
+        public StudentController(Messages messages)
         {
-            _unitOfWork = unitOfWork;
             _messages = messages;
-            _studentRepository = new StudentRepository(unitOfWork);
-            _courseRepository = new CourseRepository(unitOfWork);
         }
 
         [HttpGet]
@@ -36,104 +28,43 @@ namespace Api.Controllers
         [HttpPost]
         public IActionResult Register([FromBody] NewStudentDto dto)
         {
-            var student = new Student(dto.Name, dto.Email);
+            var command = new RegisterCommand(dto.Name, dto.Email, dto.Course1, dto.Course1Grade, dto.Course2, dto.Course2Grade);
 
-            if (dto.Course1 != null && dto.Course1Grade != null)
-            {
-                Course course = _courseRepository.GetByName(dto.Course1);
-                student.Enroll(course, Enum.Parse<Grade>(dto.Course1Grade));
-            }
+            Result result = _messages.Dispatch(command);
 
-            if (dto.Course2 != null && dto.Course2Grade != null)
-            {
-                Course course = _courseRepository.GetByName(dto.Course2);
-                student.Enroll(course, Enum.Parse<Grade>(dto.Course2Grade));
-            }
-
-            _studentRepository.Save(student);
-            _unitOfWork.Commit();
-
-            return Ok();
+            return FromResult(result);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Unregister(long id)
         {
-            Student student = _studentRepository.GetById(id);
-            if (student == null)
-                return Error($"No student found for Id {id}");
+            Result result = _messages.Dispatch(new UnregisterCommand(id));
+            return FromResult(result);
 
-            _studentRepository.Delete(student);
-            _unitOfWork.Commit();
-
-            return Ok();
         }
 
         [HttpPost("{id}/enrollments/{enrollmentNumber}/deletion")]
         public IActionResult Disenroll(long id, int enrollmentNumber, [FromBody] StudentDisenrollmentDto dto)
         {
-            Student student = _studentRepository.GetById(id);
-            if (student == null)
-                return Error($"No student found for Id {id}");
+            Result result = _messages.Dispatch(new DisenrollCommand(id, enrollmentNumber, dto.Comment));
+            return FromResult(result);
 
-            if (string.IsNullOrWhiteSpace(dto.Comment))
-                return Error("Disenrollment comment is required");
-
-            Enrollment enrollment = student.GetEnrollment(enrollmentNumber);
-
-            student.RemoveEnrollment(enrollment, dto.Comment);
-
-            _unitOfWork.Commit();
-
-            return Ok();
         }
 
 
         [HttpPut("{id}/enrollments/{enrollmentNumber}")]
         public IActionResult Transfer(long id, int enrollmentNumber, [FromBody] StudentTransferDto dto)
         {
-            Student student = _studentRepository.GetById(id);
-            if (student == null)
-                return Error($"No student found for Id {id}");
+            Result result = _messages.Dispatch(new TransferCommand(id, enrollmentNumber, dto.Course, dto.Grade));
+            return FromResult(result);
 
-            Course course = _courseRepository.GetByName(dto.Course);
-            if (course == null)
-                return Error($"Course is incorrect: '{dto.Course}'");
-
-            bool success = Enum.TryParse(dto.Grade, out Grade grade);
-            if (!success)
-                return Error($"Grade is incorrect: '{dto.Grade}'");
-
-            var enrollment = student.GetEnrollment(enrollmentNumber);
-            if (enrollment == null)
-                return Error($"No enrollment found with number '{enrollment}'");
-
-            enrollment.Update(course, grade);
-
-            _unitOfWork.Commit();
-            return Ok();
         }
 
         [HttpPost("{id}/enrollments")]
         public IActionResult Enroll(long id, [FromBody] StudentEnrollmentDto dto)
         {
-            Student student = _studentRepository.GetById(id);
-            if (student == null)
-                return Error($"No student found for Id {id}");
-
-            Course course = _courseRepository.GetByName(dto.Course);
-            if (course == null)
-                return Error($"Course is incorrect: '{dto.Course}'");
-
-            bool success = Enum.TryParse(dto.Grade, out Grade grade);
-            if (!success)
-                return Error($"Grade is incorrect: '{dto.Grade}'");
-
-            student.Enroll(course, grade);
-
-            _unitOfWork.Commit();
-
-            return Ok();
+            Result result = _messages.Dispatch(new EnrollCommand(id, dto.Course, dto.Grade));
+            return FromResult(result);
 
         }
 
@@ -143,7 +74,8 @@ namespace Api.Controllers
             var command = new EditPersonalInfoCommand(id, dto.Email, dto.Name);
 
             Result result = _messages.Dispatch(command);
-            return result.IsSuccess ? Ok() : Error(result.Error);
+            return FromResult(result);
+
         }
     }
 }
--- Before/src/Logic/Students/EditPersonalInfoCommand.cs
+++ Before/src/Logic/Students/EditPersonalInfoCommand.cs
@@ -1,8 +1,7 @@
 ﻿using CSharpFunctionalExtensions;
-using FluentNHibernate;
 using Logic.Dtos;
 using Logic.Utils;
-using NHibernate.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -20,7 +19,7 @@ namespace Logic.Students
 
     public interface IQueryHandler<TQuery, TResult> where TQuery : IQuery<TResult>
     {
-        TResult Handle (TQuery query);
+        TResult Handle(TQuery query);
     }
 
     public sealed class GetListQuery : IQuery<List<StudentDto>>
@@ -80,13 +79,13 @@ namespace Logic.Students
         public EditPersonalInfoCommand(long id, string name, string email)
         {
             Id = id;
-            Name = name; 
+            Name = name;
             Email = email;
         }
 
-        public long Id { get;  }
-        public string Name { get;  }
-        public string Email { get;  }
+        public long Id { get; }
+        public string Name { get; }
+        public string Email { get; }
     }
 
 
@@ -114,4 +113,243 @@ namespace Logic.Students
             return Result.Ok();
         }
     }
+
+    public sealed class RegisterCommand : ICommand
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course1 { get; set; }
+        public string Course1Grade { get; set; }
+        public string Course2 { get; set; }
+        public string Course2Grade { get; set; }
+
+        public RegisterCommand(string name, string email, string course1, string course1Grade, string course2, string course2Grade)
+        {
+            Name = name;
+            Email = email;
+            Course1 = course1;
+            Course1Grade = course1Grade;
+            Course2 = course2;
+            Course2Grade = course2Grade;
+        }
+    }
+
+    public sealed class RegisterCommandHandler : ICommandHandler<RegisterCommand>
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public RegisterCommandHandler(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Result Handle(RegisterCommand command)
+        {
+            var courseRepository = new CourseRepository(_unitOfWork);
+            var studentRepository = new StudentRepository(_unitOfWork);
+
+            var student = new Student(command.Name, command.Email);
+
+            if (command.Course1 != null && command.Course1Grade != null)
+            {
+                Course course = courseRepository.GetByName(command.Course1);
+                student.Enroll(course, Enum.Parse<Grade>(command.Course1Grade));
+            }
+
+            if (command.Course2 != null && command.Course2Grade != null)
+            {
+                Course course = courseRepository.GetByName(command.Course2);
+                student.Enroll(course, Enum.Parse<Grade>(command.Course2Grade));
+            }
+
+            studentRepository.Save(student);
+            _unitOfWork.Commit();
+
+            return Result.Ok();
+        }
+    }
+
+    public sealed class UnregisterCommand : ICommand
+    {
+        public long Id { get; }
+
+        public UnregisterCommand(long id)
+        {
+            Id = id;
+        }
+    }
+
+    public sealed class UnregisterCommandHandler : ICommandHandler<UnregisterCommand>
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public UnregisterCommandHandler(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Result Handle(UnregisterCommand command)
+        {
+            var repository = new StudentRepository(_unitOfWork);
+            Student student = repository.GetById(command.Id);
+            if (student == null)
+            {
+                return Result.Fail($"No student found for Id {command.Id}");
+            }
+
+            repository.Delete(student);
+            _unitOfWork.Commit();
+
+            return Result.Ok();
+        }
+    }
+
+    public sealed class EnrollCommand : ICommand
+    {
+        public long Id { get; }
+        public string Course { get; }
+        public string Grade { get; }
+
+        public EnrollCommand(long id, string course, string grade)
+        {
+            Id = id;
+            Course = course;
+            Grade = grade;
+        }
+    }
+
+    public sealed class EnrollCommandHandler : ICommandHandler<EnrollCommand>
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public EnrollCommandHandler(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Result Handle(EnrollCommand command)
+        {
+            var courseRepository = new CourseRepository(_unitOfWork);
+            var studentRepository = new StudentRepository(_unitOfWork);
+            Student student = studentRepository.GetById(command.Id);
+            if (student == null)
+            {
+                return Result.Fail($"No student found with Id '{command.Id}'");
+            }
+
+            Course course = courseRepository.GetByName(command.Course);
+            if (course == null)
+            {
+                return Result.Fail($"Course is incorrect: '{command.Course}'");
+            }
+
+            bool success = Enum.TryParse(command.Grade, out Grade grade);
+            if (!success)
+            {
+                return Result.Fail($"Grade is incorrect: '{command.Grade}'");
+            }
+
+            student.Enroll(course, grade);
+
+            _unitOfWork.Commit();
+
+            return Result.Ok();
+        }
+    }
+
+    public sealed class TransferCommand : ICommand
+    {
+        public long Id { get; }
+        public int EnrollmentNumber { get; }
+        public string Course { get; }
+        public string Grade { get; }
+
+        public TransferCommand(long id, int enrollmentNumber, string course, string grade)
+        {
+            Id = id;
+            EnrollmentNumber = enrollmentNumber;
+            Course = course;
+            Grade = grade;
+        }
+    }
+
+    public sealed class TransferCommandHandler : ICommandHandler<TransferCommand>
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public TransferCommandHandler(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Result Handle(TransferCommand command)
+        {
+            var courseRepository = new CourseRepository(_unitOfWork);
+            var studentRepository = new StudentRepository(_unitOfWork);
+            Student student = studentRepository.GetById(command.Id);
+            if (student == null)
+                return Result.Fail($"No student found for Id {command.Id}");
+
+            Course course = courseRepository.GetByName(command.Course);
+            if (course == null)
+                return Result.Fail($"Course is incorrect: '{command.Course}'");
+
+            bool success = Enum.TryParse(command.Grade, out Grade grade);
+            if (!success)
+                return Result.Fail($"Grade is incorrect: '{command.Grade}'");
+
+            var enrollment = student.GetEnrollment(command.EnrollmentNumber);
+            if (enrollment == null)
+                return Result.Fail($"No enrollment found with number '{enrollment}'");
+
+            enrollment.Update(course, grade);
+
+            _unitOfWork.Commit();
+            return Result.Ok();
+        }
+    }
+
+    public sealed class DisenrollCommand : ICommand
+    {
+        public long Id { get;  }
+        public int EnrollmentNumber { get; }
+        public string Comment { get; }
+
+        public DisenrollCommand(long id, int enrollmentNumber, string comment)
+        {
+            Id = id;
+            EnrollmentNumber = enrollmentNumber;
+            Comment = comment;
+        }
+    }
+
+    public sealed class DisenrollCommandHandler : ICommandHandler<DisenrollCommand>
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public DisenrollCommandHandler(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Result Handle(DisenrollCommand command)
+        {
+            var studentRepository = new StudentRepository(_unitOfWork);
+
+            Student student = studentRepository.GetById(command.Id);
+            if (student == null)
+                return Result.Fail($"No student found for Id {command.Id}");
+
+            if (string.IsNullOrWhiteSpace(command.Comment))
+                return Result.Fail("Disenrollment comment is required");
+
+            Enrollment enrollment = student.GetEnrollment(command.EnrollmentNumber);
+
+            student.RemoveEnrollment(enrollment, command.Comment);
+
+            _unitOfWork.Commit();
+
+            return Result.Ok();
+        }
+    }
+
 }
